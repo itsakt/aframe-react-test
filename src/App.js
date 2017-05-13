e919a28edb5556c80fc44243b6d696cfaa6b37f0
akt <akt@pytenlabs.com>
2017-05-13 15:41:53 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { render } from 'react-dom'
+import 'aframe'
+import { Scene, Entity } from 'aframe-react'
+
+import img1 from './images/1.jpg'
+import img2 from './images/2.jpg'
+import img3 from './images/3.jpg'
+import img4 from './images/4.jpg'
+import img5 from './images/5.jpg'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: 0,
+      images: [
+        {
+          src: img1
+        },
+        {
+          src: img2
+        },
+        {
+          src: img3
+        },
+        {
+          src: img4
+        },
+        {
+          src: img5
+        }
+      ]
+    }
+
+    this._onClickNext = this._onClickNext.bind(this)
+  }
+  _onClickNext() {
+    let state = this.state
+    state.active = state.active === state.images.length - 1 ? 0 : state.active + 1
+    this.setState(state)
+  }
+  render() {
+    return (
+      <div>
+        <Scene shadow vr-mode-ui="enabled: true">
+          <a-assets />
+          <Entity primitive="a-sky" src={this.state.images[this.state.active].src} />
+        </Scene>
+
+        <button
+          style={{
+            position: 'absolute',
+            zIndex: 100,
+            top: 0,
+            left: 0
+          }}
+          onClick={this._onClickNext}
+        >
+          NEXT
+        </button>
+      </div>
+    )
+  }
+}
+
+export default App
